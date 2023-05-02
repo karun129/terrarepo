c5c88b5396c75710cbf377d675bba6b6cd49d4b7
karun129 <117799841+karun129@users.noreply.github.com>
2023-05-02 11:35:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIA4QUKASHDSEKLAZKU"
-  secret_key = "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"
+  access_key = "AKIA6GX5KJDXTWWEMTW6"
+  secret_key = "6NptWikFOJKHW/K9Fm1lZFcM8Ax6ZlMu4Es25Ufr"
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-0e6329e222e662a52"
   instance_type = var.instance_type
   tags= {
-    Name = "raham"
+    Name = "karun"
   }
 }
 
